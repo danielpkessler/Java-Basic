5723726e2e990fef3233511db8bdd4aab49ba3bb
Daniel Kessler <147793106+danielpkessler@users.noreply.github.com>
2023-10-17 23:01:43 -0600

    Add files via upload

---
+++ Person Information Management.java
@@ -0,0 +1,240 @@
+/*
+ 
+Fichier TP 3 - C
+
+Auteur : Daniel Kessler
+Cours IFT 1810 - section A
+But du programme :
+
+1. Écrire deux contructeurs pour instancier trois personnes différentes.
+2. Écrire la méthode afficher et des méthodes d'accès pour afficher les informations de la deuxième personne.
+3. Afficher le contenu du tableau pers.
+4. Déterminer et afficher les informations de la personne qui consomme le plus de café parmi les cinq personnes.
+5. Réduire de une tasse la consommation de café pour les personnes qui consomment au moins une tasse de café par jour.
+6. Reafficher le tableau après la modification.
+7. Compter et afficher le nombre de personnes nées en mai.
+
+ */
+
+package udem;
+
+
+class Personne {
+	
+	private String naissance;
+	private int nbCafe;
+	
+	// Premier Constructeur.
+	public Personne (String naissance, int nbCafe)
+	{
+		this.naissance = naissance;
+		this.nbCafe = nbCafe;
+	}
+	
+	
+	// Deuxième Constructeur.
+	public Personne (String naissance)
+	{
+		this.naissance = naissance;
+		nbCafe = 1;
+	}
+	
+	
+	// Méthodes d'accès pour la naissance.
+	public int RangMois() {
+		return Integer.parseInt(naissance.substring(3, 5));
+	}
+	
+	public String getNaissance() {
+		return naissance;
+	}
+	
+	public void setNbCafe(int nbCafe) {
+		this.nbCafe = nbCafe;
+	}
+	
+	public String getMois() {
+		String[] nomMois = {"Janvier", "Fevrier", "Mars", "Avril", "Mai","Juin",
+			"Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre"};
+		return nomMois [RangMois() - 1];
+	}
+	
+	public int getJour() {
+		return Integer.parseInt(naissance.substring(0, 2));
+	}
+	
+	public int getAnnee() {
+		return Integer.parseInt(naissance.substring(6));
+	}
+	
+	
+	// Méthode d'accès a la consommation de café par jour.
+	public int getNbCafe() {
+		return nbCafe;
+	}
+
+	
+	// Fonction pour afficher les informations d'une personne choisie.
+	public void affichPersVoulue(String en_tete)
+	{
+		System.out.printf(en_tete);
+		System.out.printf("	Naissance : %d %s %d \n", getJour(), getMois(), getAnnee());
+		System.out.printf("	Cafe      : %d tasse(s) par jour\n", getNbCafe());
+		System.out.printf("\n");
+	}
+
+	// Méthode de modification pour la consommation de café.
+	public void setConsoCafe(int nouvCafe) {
+		nbCafe = nouvCafe;
+	}
+		
+}
+	
+public class TestPersonne {
+	
+	// Fonction pour afficher les tableaux de 5 personnes choisies.
+	static void afficher(Personne[] pers, int nbPers, String message) {
+		System.out.printf("	Indice	      Tableau pers\n");
+		System.out.printf("---------------------------------------------------------------\n");
+		
+		for(int i = 0; i < nbPers; i++) {
+			if(pers[i].getNbCafe() > 1) { 
+				System.out.printf("	%3d %20s %15s tasses\n", i, 
+					pers[i].getNaissance(), pers[i].getNbCafe()); }
+			
+			else if(pers[i].getNbCafe() == 1) {
+				System.out.printf("	%3d %20s %15s tasse (par défaut)\n", i, 
+					pers[i].getNaissance(), pers[i].getNbCafe()); }
+			
+			else if(pers[i].getNbCafe() == 0) {
+				System.out.printf("	%3d %20s %15s tasse\n", i, 
+						pers[i].getNaissance(), pers[i].getNbCafe()); }
+		}
+	}
+	
+	/* Méthode de modification - réduction de 1 tasse de café 
+	par jour pour les personnes qui consomment du café. */
+	static void moins(Personne[] personnes) { 
+		for(int i = 0; i < personnes.length; i++) {
+			if(personnes[i].getNbCafe() > 0) {
+				personnes[i].setNbCafe(personnes[i].getNbCafe() - 1); }
+		}
+	}
+	
+	// Fonction pour déterminer la consomation maximale de café.
+	static Personne consMaxCafe(Personne[] tableau, int nbPers) {
+		Personne plusGrand = tableau[0];
+		for (int i = 0; i < nbPers; i++) {
+			if (tableau[i].getNbCafe() > plusGrand.getNbCafe()) {
+				plusGrand = tableau[i];
+			}
+		}
+		return plusGrand;
+	}
+	
+	// Fonction pour trouver le nombre de personnes nées dans un mois choisi.
+	static int moisVoulu (Personne[] pers, int nbPers, String moisDonne) {
+		int mois = 0;
+		for (int i = 0; i < nbPers; i++) {
+			if (pers[i].getMois() == moisDonne) {
+				mois++; }
+		}
+		
+		return mois;
+}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	// Instanciation des trois premières personnes.
+		
+		Personne pers1 = new Personne ("19/05/1997", 4),
+			 pers2 = new Personne ("31/12/1990"), // par defaut 1 tasse de café
+			 pers3 = new Personne ("08/05/1994", 2);
+		
+		
+	// 1. Affichage des informations de la deuxième personne.
+
+		pers2.affichPersVoulue("Informations de la deuxième personne :\n");
+		
+		
+	// 2. Tableau des 5 nouvelles personnes.
+		
+		Personne[] pers = { new Personne("16/05/2001", 2), 
+				    new Personne("02/05/1990"),
+				    new Personne("23/11/1996", 5), 
+				    new Personne("19/02/2000", 0), 
+				    new Personne("30/05/1991", 2) };
+		int nbPers = pers.length;
+		
+		System.out.printf("\nAffichage du contenu du tableau pers au début :\n\n");
+		afficher(pers, nbPers, "");
+		
+		
+	// 3. Détermination et affichage des informations de la personne qui consomme le plus de café.
+		
+		consMaxCafe(pers, nbPers).affichPersVoulue("\n\nInformations de la personne qui consomme le plus de cafe :\n");
+		
+	// 4. Réduire de 1 tasse la consommation de café des personnes en consomment et re-afficher le tableau.
+		
+		System.out.printf("\nReaffichage du contenu du tableau après la modification.\n");
+		System.out.printf("Réduction de 1 tasse de café pour les personnes qui consomment :\n\n");
+		moins(pers);
+		afficher(pers, nbPers, "");
+		
+	// 5. Compter et afficher le nombre de personnes nées en mai.
+		
+		System.out.printf("\n\nIl y a %d personne(s) nées en mai.\n", moisVoulu(pers, nbPers, "Mai"));
+		
+	}
+		
+}
+
+
+/*
+
+
+
+EXECUTION :
+
+
+Informations de la deuxième personne :
+	Naissance : 31 Decembre 1990 
+	Cafe      : 1 tasse(s) par jour
+
+
+Affichage du contenu du tableau pers au début :
+
+	Indice	      Tableau pers
+---------------------------------------------------------------
+	  0           16/05/2001               2 tasses
+	  1           02/05/1990               1 tasse (par défaut)
+	  2           23/11/1996               5 tasses
+	  3           19/02/2000               0 tasse
+	  4           30/05/1991               2 tasses
+
+
+Informations de la personne qui consomme le plus de cafe :
+	Naissance : 23 Novembre 1996 
+	Cafe      : 5 tasse(s) par jour
+
+
+Reaffichage du contenu du tableau après la modification.
+Réduction de 1 tasse de café pour les personnes qui consomment :
+
+	Indice	      Tableau pers
+---------------------------------------------------------------
+	  0           16/05/2001               1 tasse (par défaut)
+	  1           02/05/1990               0 tasse
+	  2           23/11/1996               4 tasses
+	  3           19/02/2000               0 tasse
+	  4           30/05/1991               1 tasse (par défaut)
+
+
+Il y a 3 personne(s) nées en mai.
+
+
+
+ */
---
+++ Telephone Number Analysis .java
@@ -0,0 +1,155 @@
+/*
+ 
+Fichier TP 3 - B
+
+Auteur : Daniel Kessler
+Cours IFT 1810 - section A
+But du programme :
+
+1. Afficher le numero de telephone de l'UDM et de Jean sous format (XXX) XXX-XXXX
+2. Afficher et compter combien de fois on trouve les chiffres 2 et 4 dans le telephone de l'UDM et combien de fois on retrouve le chiffre 2 dans le telephone de Jean.
+3. Compter combien de fois on retrouve des chiffres impairs dans le telephone de l'UDM et combien de fois on retrouve des chiffres pairs dans le telephone de Jean.
+4. Determiner et afficher les chiffres impairs communs des deux numeros de telephone.
+
+ */
+
+
+public class TP3B {
+
+	// Fonction pour compter et afficher
+	
+	static int compteur(String chiffreVoulu, String numeroTel)	{ 
+		int compteur = 0;
+		for(int i = 0; i < numeroTel.length(); i++) {
+			if (chiffreVoulu.equals(String.valueOf(numeroTel.charAt(i)))) {
+				compteur++; }
+		}
+		return compteur;
+	}
+	
+	
+	// Fonction pour compter et afficher les chiffres impairs
+	
+	static void afficherImpairs(String telephone, String nom)
+	{
+		System.out.printf("\nLes chiffres impairs dans %10s sont : \n", nom);
+		for (char c = '1'; c <= '9'; c+=2) {
+			if (telephone.indexOf(c) >= 0) {
+				System.out.printf(" %c \n", c); }
+			}
+	}
+	
+	
+	// Fonction pour compter et afficher les chiffres pairs
+	
+	static void afficherPairs(String telephone, String nom)
+	{
+		System.out.printf("\nLes chiffres pairs dans %10s sont : \n", nom);
+		for (char c = '0'; c <= '8'; c+=2) {
+			if (telephone.indexOf(c) >= 0) {
+				System.out.printf(" %c \n", c); }
+			}
+	}
+	
+	
+	// Fonction pour compter le nombre de chiffres impairs
+	
+	static int compteurImpair(String telephone)
+	{
+		int compteurImpair = 0;
+		String impairs = "13579";
+		for (int i = 0; i < telephone.length(); i++) {
+			if (impairs.indexOf(telephone.charAt(i)) >= 0) {
+				compteurImpair++;
+			}
+		}
+		return compteurImpair; 
+	}
+	
+	
+	// Fonction pour compter le nombre de chiffres pairs
+	
+	static int compteurPair(String telephone)
+	{
+		int compteurPair = 0;
+		String pairs = "02468";
+		for (int i = 0; i < telephone.length(); i++) {
+			if (pairs.indexOf(telephone.charAt(i)) >= 0) {
+				compteurPair++;
+			}
+		}
+		return compteurPair; 
+	}
+	
+	
+	// Fonction pour determiner et afficher les chiffres impairs communs des deux numeros de telephone
+	
+	static void afficherImpairsCommuns(String telUDM, String telJean)
+	{
+		System.out.printf("\nLes chiffres impairs communs des deux telephones sont : \n");
+		for(char c = '1' ; c <= '9'; c+=2) {
+			if(telUDM.indexOf(c) >= 0 && telJean.indexOf(c) >= 0) {
+				System.out.printf(" %c \n", c); }
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	// 1. Extraire de sous-chaines et afficher
+	
+		String telUDM = "5143436111";
+		String codeRegionUDM = telUDM.substring(0, 3),
+				  echangeUDM = telUDM.substring(3, 6), 
+				   numeroUDM = telUDM.substring(6);
+		System.out.printf("(%s) %s-%s\n", codeRegionUDM, echangeUDM, numeroUDM);
+		
+		String telJean = "4501897654";
+		String codeRegionJean = telJean.substring(0, 3),
+				  echangeJean = telJean.substring(3, 6),
+				   numeroJean = telJean.substring(6);
+		System.out.printf("(%s) %s-%s\n", codeRegionJean, echangeJean, numeroJean);
+
+	// 2. Compter et afficher :
+		
+		System.out.printf("\nIl y a %d fois le chiffre '3' dans le numero de telephone de l'UdM.\n", compteur("3", telUDM));
+		System.out.printf("Il y a %d fois le chiffre '1' dans le numero de telephone de l'UdM.\n", compteur("1", telUDM));
+		System.out.printf("Il y a %d fois le chiffre '2' dans le numero de telephone de Jean.\n", compteur("2", telJean));
+
+	// 3. Compter et afficher le nombre de chiffres impairs du telephone de l'UdM et les chiffres pairs du telephone de Jean.
+		
+		//System.out.printf("Il y a %d chiffres impairs dans le numero de telephone de l'UdM.\n", compteurImpair(telUDM, "ggg"));
+		System.out.printf("\nIl y a %d fois des chiffres impairs dans %10s. \n", compteurImpair(telUDM), "le telephone de l'UDM");
+		System.out.printf("Il y a %d fois des chiffres pairs dans %10s. \n", compteurPair(telJean), "le telephone de Jean");
+		
+	// 4. Determiner et afficher les chiffres impairs communs des deux numeros de telephone
+		
+		afficherImpairsCommuns(telUDM, telJean);
+		
+	}
+
+}
+
+/*
+  
+ EXECUTION :
+ 
+ 
+(514) 343-6111
+(450) 189-7654
+
+Il y a 2 fois le chiffre '3' dans le numero de telephone de l'UdM.
+Il y a 4 fois le chiffre '1' dans le numero de telephone de l'UdM.
+Il y a 0 fois le chiffre '2' dans le numero de telephone de Jean.
+
+Il y a 7 fois des chiffres impairs dans le telephone de l'UDM. 
+Il y a 5 fois des chiffres pairs dans le telephone de Jean. 
+
+Les chiffres impairs communs des deux telephones sont : 
+ 1 
+ 5 
+
+
+*/
